11dcd2ae5cab9570478ced82e8badc97806d7153
alexander.bespalov <abespalv@gmail.com>
2017-06-30 12:03:24 +0300

    javac compiler studio 3.0 realm to be added

--- MVPedu/app/src/main/java/com/example/alexanderbespalov/mvpedu/presentation/view/ex1/ShowMsgView.java
+++
@@ -1,7 +0,0 @@
-package com.example.alexanderbespalov.mvpedu.presentation.view.ex1;
-
-import com.arellomobile.mvp.MvpView;
-
-public interface ShowMsgView extends MvpView {
-    void showMessage(int message);
-}
--- MVPedu/app/src/main/java/com/example/alexanderbespalov/mvpedu/presentation/view/main/MainView.java
+++
@@ -1,8 +0,0 @@
-package com.example.alexanderbespalov.mvpedu.presentation.view.main;
-
-import com.arellomobile.mvp.MvpView;
-
-public interface MainView extends MvpView {
-
-
-}
